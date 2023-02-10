5f73da457be3a74e8db4c78aa58a01fdb023432e
Chris Buonocore <chrisdistrict@gmail.com>
2023-02-09 23:31:14 -0500

    add base survey

---
+++ app/src/main/java/com/solvays/ui/survey/SurveyFragment.kt
@@ -0,0 +1,38 @@
+package com.solvays.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.solanamobile.seedvault.Wallet
+import com.solvays.databinding.FragmentGalleryBinding
+import com.solvays.databinding.FragmentSurveyBinding
+
+class SurveyFragment : Fragment() {
+
+    private var _binding: FragmentSurveyBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentSurveyBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
